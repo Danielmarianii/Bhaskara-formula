27d14b7342270d0bdbc30bba8ab66e3c198aee7f
“danielmarianii” <danielmariani10@hotmail.com>
2021-03-21 17:42:18 -0300

    update

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		// delta = Math.pow(b, 2.0) - 4*a*c;
+		// project delta = Math.pow(b, 2.0) - 4*a*c;
 		// x1 = (-b + Math.sqrt(delta)) / (2.0 * a);
 		// x2 = (-b - Math.sqrt(delta)) / (2.0 * a);
 
