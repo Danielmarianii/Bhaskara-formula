48fcfe439f1521cad8806a722b305ad951b4793e
“danielmarianii” <danielmariani10@hotmail.com>
2021-07-15 22:08:37 -0300

    update

--- src/Main.java
+++ src/Main.java
@@ -10,19 +10,19 @@ public class Main {
 		System.out.println("Type the values:");
 		Scanner sc = new Scanner(System.in);
 
-		System.out.println("a=");
+		System.out.print("a=");
 		double a = sc.nextDouble();
-		int a_convert = (int) a;
+		int a_int = (int) a;
 
-		System.out.println("b=");
+		System.out.print("b=");
 		double b = sc.nextDouble();
-		int b_convert = (int) b;
+		int b_int = (int) b;
 
-		System.out.println("c=");
+		System.out.print("c=");
 		double c = sc.nextDouble();
-		int c_convert = (int) c;
+		int c_int = (int) c;
 
-		double step1delta = Math.pow(b_convert, 2.0);
+		double step1delta = Math.pow(b_int, 2.0);
 		int step1 = (int) step1delta;
 
 		double step2delta = -4 * a * c;
@@ -31,9 +31,11 @@ public class Main {
 		double delta = Math.pow(b, 2.0) - 4 * a * c;
 		
 		
-		System.out.printf("delta= " + step1 + "-4" + "." + a_convert + "." + c_convert + "%n");
+		System.out.printf("delta= " + step1 + "-4" + "." + a_int + "." + c_int + "%n");
 		System.out.println("delta= " + step1 + " " + step2);
 		
+		double x1 =0;
+		double x2=0;
 		if (delta < 0) {
 			System.out.printf("delta= " + (int)delta + "%nImpossible to resolve with a negative delta.");
 		}
@@ -42,17 +44,17 @@ public class Main {
 			System.out.println("delta= " + (int) delta);
 			System.out.println();
 	
-			double x1 = (-b + Math.sqrt(delta)) / (2.0 * a);
+			x1 = (-b + Math.sqrt(delta)) / (2.0 * a);
 	
-			System.out.println("x1= " + (-b_convert) + " " + (Math.sqrt(delta)) + " /2." + a_convert);
-			System.out.println("x1= " + ((-b_convert) + (Math.sqrt(delta))) + " /" + 2 * a_convert);
+			System.out.println("x1= " + (-b_int) + " " + (Math.sqrt(delta)) + " /2." + a_int);
+			System.out.println("x1= " + ((-b_int) + (Math.sqrt(delta))) + " /" + 2 * a_int);
 			System.out.println("x1= " + x1);
 			System.out.println();
 	
-			double x2 = (-b - Math.sqrt(delta)) / (2.0 * a);
+			x2 = (-b - Math.sqrt(delta)) / (2.0 * a);
 	
-			System.out.println("x2= " + (-b_convert) + " " + -(Math.sqrt(delta)) + " /2." + a_convert);
-			System.out.println("x2= " + ((-b_convert) - (Math.sqrt(delta))) + " /" + 2 * a_convert);
+			System.out.println("x2= " + (-b_int) + " " + -(Math.sqrt(delta)) + " /2." + a_int);
+			System.out.println("x2= " + ((-b_int) - (Math.sqrt(delta))) + " /" + 2 * a_int);
 			System.out.println("x2= " + x2);
 		}
 		sc.close();
